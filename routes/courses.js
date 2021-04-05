5c57f03f8851c4d9e9beb371589f512fd3c507e1
Manoel Blue <manoelbluee@gmail.com>
2021-04-05 10:21:59 +0200

    remove created course message

--- routes/courses.js
+++ routes/courses.js
@@ -54,10 +54,7 @@ router.post('/courses', authenticateUser, asyncHandler(async (req, res) => {
     let course;
     try {
         course = await Courses.create(req.body);
-        res.status(201).location(`/courses/${course.id}`)
-            .json({
-                "message": "Course created successfully"
-            }).end();
+        res.status(201).location(`/courses/${course.id}`).end();
     } catch (error) {
         if(error.name === "SequelizeValidationError") {
             const errors = error.errors.map(error => error.message);
